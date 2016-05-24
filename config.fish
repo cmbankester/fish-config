066e17c0201c4b832a27983afaa3624103129251
Christian Michael Paul Bankester <cbankester@immense.net>
2016-05-24 11:10:49 -0500

    Fixed issue with dirname not being called correctly

--- config.fish
+++ config.fish
@@ -8,29 +8,12 @@ fundle init
 
 set -gx PATH /usr/local/sbin $PATH
 
-test -e (dirname $argv[1])/config.local.fish; and source (dirname $argv[1])/config.local.fish
+test -e (dirname (status -f))/config.local.fish; and source (dirname (status -f))/config.local.fish
 
 # Load desks
 test -n "$DESK_ENV"; and . "$DESK_ENV"; or true
 
-set -l newline \x0a
-set -l tab \x09
-set -l green_s \x1b\x5b32m
-set -l green_e \x1b\x5b30m\x1b\x28B\x1b\x5bm
-
 nvm use default >/dev/null
-
-if which docker-machine >/dev/null
-  set -l docker_machine_status (docker-machine status vmware-fusion)
-  if [ $docker_machine_status = "Running" ]
-    eval (docker-machine env vmware-fusion)
-    set fish_greeting "Docker Machine and Docker are ready to use"
-  else
-    set fish_greeting "Docker machine status:$tab$docker_machine_status"$newline"Type$green_s docker-machine start vmware-fusion; and eval (docker-machine env vmware-fusion)$green_e to start"
-  end
-else
-  set fish_greeting "Docker Machine is not installed"
-end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
